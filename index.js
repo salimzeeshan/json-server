8fd3177483a48a863050e839ef1b9771f3a3f899
salimzeeshan <zeeshasalim541@gmail.com>
2023-04-21 15:12:35 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const json = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
+const server = json.create()
+const router = json.router(path.join(__dirname, 'db.json'))
+const middlewares = json.defaults()
+
+server.use(cors())
+server.use(json.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
